ac80421258c479d8c2467c0c81c220dd616bddbd
vinodvr81 <vinod.vukkalam@gmail.com>
2020-12-03 14:43:01 +0530

    removed one bug

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -24,7 +24,6 @@ from .models import Choice, Question
 #     }
 #     return HttpResponse(template.render(context, request))
 
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -41,25 +40,26 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'polls/details.html'
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
