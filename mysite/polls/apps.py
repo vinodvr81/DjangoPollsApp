ec978c26c877b356b20f93b439661dcfc0cf8c80
vinodvr81 <vinod.vukkalam@gmail.com>
2020-12-04 00:20:01 +0530

    Added Style sheets and test cases too

--- mysite/polls/apps.py
+++ mysite/polls/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PollsConfig(AppConfig):
     name = 'polls'
-
+    #verbose_name = 'Polls Name'
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,6 +9,7 @@ class Meta:
     app_label  = 'polls'
 
 class Question(models.Model):
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     # It’s important to add __str__() methods to the models, not only for your own convenience when dealing with the
@@ -17,9 +18,23 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # def create_question(question_text, days):
+    #     """
+    #     Create a question with the given `question_text` and published the
+    #     given number of `days` offset to now (negative for questions published
+    #     in the past, positive for questions that have yet to be published).
+    #     """
+    #     time = timezone.now() + datetime.timedelta(days=days)
+    #     return Question.objects.create(question_text=question_text, pub_date=time)
+
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
